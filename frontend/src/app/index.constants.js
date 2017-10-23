4d92721340250e0119ac10479fd18f4384c2b7c9
mosipov <maxim.osipov@stud.htwk-leipzig.de>
2017-10-23 20:23:39 +0200

    Frontend

---
+++ frontend/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('workshopWeb')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
