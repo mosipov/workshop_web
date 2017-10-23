1809ff8ee6e535bb57da1d230a49ac8add0e5e45
mosipov <maxim.osipov@stud.htwk-leipzig.de>
2017-10-24 00:06:51 +0200

    deleted

--- frontend/src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('workshopWeb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngRoute', 'ngMaterial', 'toastr']);
-
-})();
